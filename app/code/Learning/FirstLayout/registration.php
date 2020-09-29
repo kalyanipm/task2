d3364ef6b8c53781177470f6bc9be567225f6ea9
kalyanipm <kalyani.pagare@afourtech.com>
2020-09-29 16:37:36 +0530

    Module development code only app folder

---
+++ app/code/Learning/FirstLayout/registration.php
@@ -0,0 +1,11 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Learning_FirstLayout',
+    __DIR__
+);
+
+?>
---
+++ app/etc/env.php
@@ -0,0 +1,51 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_1egzjo'
+    ],
+    'crypt' => [
+        'key' => 'b78528927166f3ecbc71a7181b64c2ef'
+    ],
+    'db' => [
+        'table_prefix' => 'mg_',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => 'root123',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Mon, 28 Sep 2020 08:00:41 +0000'
+    ]
+];
